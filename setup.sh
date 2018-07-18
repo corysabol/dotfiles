bc28ccbb99fdbc222d4a146db49598b0eeeaf281
bit0pus <cory@secureideas.com>
2018-07-18 12:49:11 -0400

    add i3 config to setup; add terminator config

--- setup.sh
+++ setup.sh
@@ -1,17 +1,16 @@
-#!/usr/bin/env sh
+#!/bin/bash
 
-if [[ ! (which wal) ]]; then
+if [[ ! $(which wal) ]]; then
   echo "ERROR: You've not got pywal installed, m8."
   exit 1
 fi
 
 echo "Sprinkling dotfiles around wherever..."
 mkdir -p ~/.i3
-cp ./confg ~/.i3/
+cp ./config ~/.i3/config
 mkdir -p ~/.config/nvim
 cp ./init.vim ~/.config/nvim/
 cp ./.xinitrc ~/.xinitrc
-cp ./.bashrc ~/.bashrc
 
 # add some wal stuff to .bashrc
 echo "(cat ~/.cache/wal/sequences &)" | tee -a ~/.bashrc
